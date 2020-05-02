145f9379094b9ae33f57a881592a5261715dde8e
けお <o.kengo1987@gmail.com>
2020-05-02 22:18:56 +0900

    レスポンシブル対応修正

--- template/common/js/main.js
+++ template/common/js/main.js
@@ -1,10 +1,9 @@
-$(function () {
-  $(function () {
-
+$(function() {
+  $(function() {
     // SPメニュー
-    $('.js-toggle-sp-menu').on('click', function () {
-      $(this).toggleClass('active');
-      $('.js-toggle-sp-menu-target').toggleClass('active');
+    $(".js-toggle-sp-menu").on("click", function() {
+      $(this).toggleClass("active");
+      $(".js-toggle-sp-menu-target").toggleClass("active");
       console.log("open");
     });
 
@@ -15,78 +14,80 @@ $(function () {
     // }
 
     // メッセージ表示
-    var $jsShowMsg = $('#js-show-msg');
+    var $jsShowMsg = $("#js-show-msg");
     var msg = $jsShowMsg.text();
     if (msg.replace(/^[\s　]+|[\s　]+$/g, "").length) {
-      $jsShowMsg.slideToggle('slow');
-      setTimeout(function () { $jsShowMsg.slideToggle('slow'); }, 5000);
+      $jsShowMsg.slideToggle("slow");
+      setTimeout(function() {
+        $jsShowMsg.slideToggle("slow");
+      }, 5000);
     }
 
     // 画像ライブプレビュー
-    var $dropArea = $('.area-drop');
-    var $fileInput = $('.input-file');
-    $dropArea.on('dragover', function (e) {
+    var $dropArea = $(".area-drop");
+    var $fileInput = $(".input-file");
+    $dropArea.on("dragover", function(e) {
       e.stopPropagation();
       e.preventDefault();
-      $(this).css('border', '3px #ccc dashed');
+      $(this).css("border", "3px #ccc dashed");
     });
-    $dropArea.on('dragleave', function (e) {
+    $dropArea.on("dragleave", function(e) {
       e.stopPropagation();
       e.preventDefault();
-      $(this).css('border', 'none');
+      $(this).css("border", "none");
     });
-    $fileInput.on('change', function (e) {
-      $dropArea.css('border', 'none');
-      var file = this.files[0],            // 2. files配列にファイルが入っています
-        $img = $(this).siblings('.prev-img'), // 3. jQueryのsiblingsメソッドで兄弟のimgを取得
-        fileReader = new FileReader();   // 4. ファイルを読み込むFileReaderオブジェクト
+    $fileInput.on("change", function(e) {
+      $dropArea.css("border", "none");
+      var file = this.files[0], // 2. files配列にファイルが入っています
+        $img = $(this).siblings(".prev-img"), // 3. jQueryのsiblingsメソッドで兄弟のimgを取得
+        fileReader = new FileReader(); // 4. ファイルを読み込むFileReaderオブジェクト
 
       // 5. 読み込みが完了した際のイベントハンドラ。imgのsrcにデータをセット
-      fileReader.onload = function (event) {
+      fileReader.onload = function(event) {
         // 読み込んだデータをimgに設定
-        $img.attr('src', event.target.result).show();
+        $img.attr("src", event.target.result).show();
       };
 
       // 6. 画像読み込み
       fileReader.readAsDataURL(file);
-
     });
 
     // テキストエリアカウント
-    var $countUp = $('#js-count'),
-      $countView = $('#js-count-view');
-    $countUp.on('keyup', function (e) {
+    var $countUp = $("#js-count"),
+      $countView = $("#js-count-view");
+    $countUp.on("keyup", function(e) {
       console.log("teset");
       $countView.html($(this).val().length);
     });
 
     // 画像切替
-    var $switchImgSubs = $('.js-switch-img-sub'),
-      $switchImgMain = $('#js-switch-img-main');
-    $switchImgSubs.on('click', function (e) {
-      $switchImgMain.attr('src', $(this).attr('src'));
+    var $switchImgSubs = $(".js-switch-img-sub"),
+      $switchImgMain = $("#js-switch-img-main");
+    $switchImgSubs.on("click", function(e) {
+      $switchImgMain.attr("src", $(this).attr("src"));
     });
 
     // お気に入り登録・削除
-    var $like,
-      likeProductId;
-    $like = $('.js-click-like') || null; //nullというのはnull値という値で、「変数の中身は空ですよ」と明示するためにつかう値
-    likeProductId = $like.data('productid') || null;
+    var $like, likeProductId;
+    $like = $(".js-click-like") || null; //nullというのはnull値という値で、「変数の中身は空ですよ」と明示するためにつかう値
+    likeProductId = $like.data("productid") || null;
     // 数値の0はfalseと判定されてしまう。product_idが0の場合もありえるので、0もtrueとする場合にはundefinedとnullを判定する
     if (likeProductId !== undefined && likeProductId !== null) {
-      $like.on('click', function () {
+      $like.on("click", function() {
         var $this = $(this);
         $.ajax({
           type: "POST",
           url: "ajaxLike.php",
           data: { productId: likeProductId }
-        }).done(function (data) {
-          console.log('Ajax Success');
-          // クラス属性をtoggleでつけ外しする
-          $this.toggleClass('active');
-        }).fail(function (msg) {
-          console.log('Ajax Error');
-        });
+        })
+          .done(function(data) {
+            console.log("Ajax Success");
+            // クラス属性をtoggleでつけ外しする
+            $this.toggleClass("active");
+          })
+          .fail(function(msg) {
+            console.log("Ajax Error");
+          });
       });
     }
   });
